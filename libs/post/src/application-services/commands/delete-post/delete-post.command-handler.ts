8065797183903143c1ed4749fe089920900a1427
Maksym Tishchuk <58475629+MaksTishchuk@users.noreply.github.com>
2023-06-18 12:49:47 +0300

    add delete post command

---
+++ libs/post/src/application-services/commands/delete-post/delete-post.command-handler.ts
@@ -0,0 +1,31 @@
+import {CommandHandler, ICommandHandler} from "@nestjs/cqrs";
+import {PostAggregate} from "@lib/post";
+import {PostRepository} from "@lib/post/providers";
+import {BadRequestException, Logger} from "@nestjs/common";
+import {DeletePostCommand} from "@lib/post/application-services/commands/delete-post/delete-post.command";
+
+@CommandHandler(DeletePostCommand)
+export class DeletePostCommandHandler implements ICommandHandler<DeletePostCommand, boolean> {
+  private readonly logger = new Logger(DeletePostCommandHandler.name)
+
+  constructor(
+    private readonly postRepository: PostRepository
+  ) {}
+
+  async execute({ id }: DeletePostCommand): Promise<boolean> {
+    const existsPost = await this.postRepository
+      .findOne(id)
+      .catch(err => {
+        this.logger.error(err)
+        return null as PostAggregate
+      })
+    if (!existsPost) {
+      throw new BadRequestException(`Post by id ${id} was not found!`)
+    }
+    const isPostDeleted = await this.postRepository.delete(id).catch(err => {
+      throw new Error(err)
+    })
+    return isPostDeleted
+  }
+
+}
