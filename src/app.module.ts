6c1c268e4236e884508ae8a0053f65a073645c58
Maksym Tishchuk <58475629+MaksTishchuk@users.noreply.github.com>
2023-06-18 15:53:19 +0300

    add controllers - post controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import {ProvidersModule} from "@lib/providers";
 import {SharedModule} from "@lib/shared";
+import {ApiModule} from "./api";
 
 @Module({
   imports: [
     SharedModule, // глобальный обработчик ошибок для всего приложения
-    ProvidersModule
+    ProvidersModule,
+    ApiModule
   ]
 })
 export class AppModule {}
