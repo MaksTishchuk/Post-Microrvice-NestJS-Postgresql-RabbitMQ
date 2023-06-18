4483709bd6e3af437c5b65b01c37bcd674f3193d
Maksym Tishchuk <58475629+MaksTishchuk@users.noreply.github.com>
2023-06-18 16:00:58 +0300

    add domains module

--- src/api/controllers/post/post.controller.ts
+++ src/api/controllers/post/post.controller.ts
@@ -1,4 +1,10 @@
 import { Controller } from '@nestjs/common';
+import {PostFacade} from "@lib/post/application-services";
 
 @Controller('post')
-export class PostController {}
+export class PostController {
+  // All our business logic realized in post facade class, so we can use it in controller without service
+
+  constructor(private readonly postFacade: PostFacade) {}
+
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import {ProvidersModule} from "@lib/providers";
 import {SharedModule} from "@lib/shared";
 import {ApiModule} from "./api";
+import { DomainsModule } from './domains/domains.module';
 
 @Module({
   imports: [
     SharedModule, // глобальный обработчик ошибок для всего приложения
     ProvidersModule,
-    ApiModule
+    ApiModule,
+    DomainsModule
   ]
 })
 export class AppModule {}
