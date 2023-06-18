d2c1d6fa9e6c2de0a55d52faf9c54fea3b52847e
Maksym Tishchuk <58475629+MaksTishchuk@users.noreply.github.com>
2023-06-18 15:31:10 +0300

    update imports

--- libs/post/src/post.module.ts
+++ libs/post/src/post.module.ts
@@ -7,6 +7,8 @@ import {POST_EVENTS_HANDLERS} from "@lib/post/application-services/events";
 import {POST_QUERIES_HANDLERS} from "@lib/post/application-services/queries";
 import {PostFacade} from "@lib/post/application-services";
 import {postFacadeFactory} from "@lib/post/providers/post-facade.factory";
+import {PostRepository} from "@lib/post/providers";
+import {PostAdapter} from "@lib/post/providers/post.adapter";
 
 @Module({
   imports: [
@@ -21,6 +23,10 @@ import {postFacadeFactory} from "@lib/post/providers/post-facade.factory";
       provide: PostFacade,
       inject: [CommandBus, QueryBus, EventBus],
       useFactory: postFacadeFactory
+    },
+    {
+      provide: PostRepository,
+      useClass: PostAdapter
     }
   ],
   exports: [PostFacade]
