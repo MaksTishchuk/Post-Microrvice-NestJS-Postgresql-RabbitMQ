10b3c3d9dc3375872c178a15318911e278b5a58c
Maksym Tishchuk <58475629+MaksTishchuk@users.noreply.github.com>
2023-06-19 14:00:56 +0300

    add get all posts

--- src/api/controllers/post/post.controller.ts
+++ src/api/controllers/post/post.controller.ts
@@ -1,8 +1,10 @@
-import {Body, Controller, Get, Param, ParseUUIDPipe, Post, UseGuards} from '@nestjs/common';
+import {Body, Controller, Get, Param, ParseUUIDPipe, Post, Query, UseGuards} from '@nestjs/common';
 import {PostFacade} from "@lib/post/application-services";
 import {CreatePostDto} from "./dto";
 import {CurrentUser, ICurrentUser, Public} from "@lib/auth";
 import {JwtGuard} from "@lib/auth/guards/jwt.guard";
+import {PaginationDto} from "@lib/shared/dto";
+import {plainToInstance} from "class-transformer";
 
 @UseGuards(JwtGuard)
 @Controller('posts')
@@ -23,5 +25,16 @@ export class PostController {
   @Get(':id')
   getPostById(@Param('id', ParseUUIDPipe) id: string) {
     return this.postFacade.queries.getPost(id)
+  }
+
+  @Public()
+  @Get()
+  async getPosts(@Query() paginationDto: PaginationDto) {
+    const pagination = plainToInstance(PaginationDto, paginationDto)
+    const result = await this.postFacade.queries.getAllPosts(pagination)
+    return {
+      ...pagination,
+      ...result
+    }
   }
 }
