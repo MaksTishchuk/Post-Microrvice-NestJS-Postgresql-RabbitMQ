9fa3cc895c4adf3ff435d4ebce67ae5f5e4727f3
Maksym Tishchuk <58475629+MaksTishchuk@users.noreply.github.com>
2023-06-19 14:39:42 +0300

    add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import {Logger, ValidationPipe} from "@nestjs/common";
+import {ConfigService} from "@nestjs/config";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || 4000
-  await app.listen(PORT, () => console.log(`Server has been started on PORT: ${PORT}!`))
+  const app = await NestFactory.create(AppModule)
+  const configService = app.get(ConfigService)
+  app.enableCors()
+  app.setGlobalPrefix('api')
+  app.useGlobalPipes(new ValidationPipe())
+  const config = new DocumentBuilder()
+    .setTitle('Posts Microservice')
+    .setDescription('Posts CRUD API')
+    .setVersion('1.0')
+    .addTag('posts')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('documentation', app, document)
+
+  const PORT = configService.get<number>('PORT', 3000)
+  await app.listen(PORT, () => {
+    Logger.log(`Application has been started on PORT: ${PORT}`, 'Main')
+    Logger.log(`Swagger documentation on: /documentation`, 'Main')
+  })
 }
 bootstrap();
